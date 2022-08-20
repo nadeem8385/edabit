80735144359e2493902f045cc8ff93ffa2a5246c
nadeem8385 <abdulnadeem4894@gmail.com>
2022-08-20 15:55:56 +0530

    modified

--- function/invert_an_array.js
+++ function/invert_an_array.js
@@ -7,5 +7,5 @@ return invertArr;
 }
 console.log(invertArray([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5])
 console.log(invertArray([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5])
-console.log(invertArray([]), [])
+console.log(invertArray([]), []);
 console.log(invertArray([0]), [0])
