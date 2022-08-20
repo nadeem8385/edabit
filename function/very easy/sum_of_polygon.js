4b57548ff802a3e360a5dfcb2fa28ff602b435e5
nadeem8385 <abdulnadeem4894@gmail.com>
2022-08-20 16:06:05 +0530

    add folder

---
+++ function/very easy/sum_of_polygon.js
@@ -0,0 +1,16 @@
+function sumPolygon(n) {
+  let a = (n - 2) * 180;
+  return a;
+}
+console.log(sumPolygon(3), 180);
+console.log(sumPolygon(4), 360);
+console.log(sumPolygon(5), 540);
+console.log(sumPolygon(6), 720);
+console.log(sumPolygon(7), 900);
+console.log(sumPolygon(8), 1080);
+console.log(sumPolygon(9), 1260);
+console.log(sumPolygon(10), 1440);
+console.log(sumPolygon(11), 1620);
+console.log(sumPolygon(12), 1800);
+console.log(sumPolygon(13), 1980);
+console.log(sumPolygon(14), 2160);
