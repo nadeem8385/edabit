50835d0d051b49d2e7d3edba1926df96077bb088
nadeem8385 <abdulnadeem4894@gmail.com>
2022-08-20 16:07:36 +0530

    removed code

--- function/basic_variable_assignment.js
+++
@@ -1,8 +0,0 @@
-function nameString(str){
-    let name = "Edabit";
-    return str + name;
-}
-console.log(nameString("Mubashir"), "MubashirEdabit")
-console.log(nameString("Matt"), "MattEdabit")
-console.log(nameString("javaScript"), "javaScriptEdabit")
-console.log(nameString("Airforce"), "Air forceEdabit")
